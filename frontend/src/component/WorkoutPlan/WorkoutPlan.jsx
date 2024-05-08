caa1f9b2d0cf9162ba9a2b6ac5928b579fcf322c
Dulana Nisayuru <IT21817762@my.sliit.lk>
2024-05-09 00:17:16 +0530

    edit css and jsx files of workoutplans

--- frontend/src/component/WorkoutPlan/WorkoutPlan.jsx
+++ frontend/src/component/WorkoutPlan/WorkoutPlan.jsx
@@ -1,163 +1,3 @@
-// import React, { useState } from 'react';
-// import jsPDF from 'jspdf';
-// import './workoutplan.css'; // Import your CSS file here
-// import DeleteIcon from './deleteBtn.png';
-// import AddIcon from './plusBtn.png' 
-
-
-// const WorkoutForm = () => {
-//   const [name, setName] = useState('');
-//   const [age, setAge] = useState('');
-//   const [weight, setWeight] = useState('');
-//   const [height, setHeight] = useState('');
-//   const [exercises, setExercises] = useState([{ exercise: '', reps: '', sets: '' }]);
-
-//   const handleAddExercise = () => {
-//     setExercises([...exercises, { exercise: '', reps: '', sets: '' }]);
-//   };
-
-//   const handleExerciseChange = (index, field, value) => {
-//     const updatedExercises = [...exercises];
-//     updatedExercises[index][field] = value;
-//     setExercises(updatedExercises);
-//   };
-
-//   const handleDeleteExercise = (index) => {
-//     const updatedExercises = [...exercises];
-//     updatedExercises.splice(index, 1);
-//     setExercises(updatedExercises);
-//   };
-
-//   const handleSaveWorkout = () => {
-//     // Logic to save workout plan
-//     console.log('Workout plan saved:', { name, age, weight, height, exercises });
-//   };
-
-//   const handleDownloadPDF = () => {
-//     // Logic to generate and download PDF
-//     const doc = new jsPDF();
-//     doc.text(20, 20, `Workout Plan: ${name}`);
-//     doc.text(20, 30, `Date: ${new Date().toLocaleDateString()}`);
-//     doc.text(20, 40, `Age: ${age}`);
-//     doc.text(20, 50, `Weight: ${weight}`);
-//     doc.text(20, 60, `Height: ${height}`);
-//     doc.text(20, 70, 'Exercises:');
-//     exercises.forEach((exercise, index) => {
-//       doc.text(30, 80 + index * 10, `${index + 1}. ${exercise.exercise}: ${exercise.reps} reps, ${exercise.sets} sets`);
-//     });
-//     doc.save('workout_plan.pdf');
-//   };
-
-//   return (
-//     <div className="PlanSharing"> {/* Add your container class here */}
-//       <div className="form-container"> {/* Add your form container class here */}
-//         <h2 className='meal-plan-form .head h2'>Create New Workout Plan</h2>
-//         <form className="form-sec workout-form"> {/* Add your form class here */}
-//           <div className="form-group"> {/* Add your form group class here */}
-//             <label htmlFor="name" className="form-label">Name:</label> {/* Add your label class here */}
-//             <input
-//               type="text"
-//               id="name"
-//               className="form-input" // Add your input class here
-//               value={name}
-//               onChange={(e) => setName(e.target.value)}
-//               required
-//             />
-//           </div>
-//           <div className="form-group">
-//             <label htmlFor="age" className="form-label">Age:</label>
-//             <input
-//               type="number"
-//               id="age"
-//               className="form-input"
-//               value={age}
-//               onChange={(e) => setAge(e.target.value)}
-//               required
-//             />
-//           </div>
-//           <div className="form-group">
-//             <label htmlFor="weight" className="form-label">Weight(kg):</label>
-//             <input
-//               type="number"
-//               id="weight"
-//               className="form-input"
-//               value={weight}
-//               onChange={(e) => setWeight(e.target.value)}
-//               required
-//             />
-//           </div>
-//           <div className="form-group">
-//             <label htmlFor="height" className="form-label">Height(cm):</label>
-//             <input
-//               type="number"
-//               id="height"
-//               className="form-input"
-//               value={height}
-//               onChange={(e) => setHeight(e.target.value)}
-//               required
-//             />
-//           </div>
-//           <div>
-//             <table className="exercise-table">
-//               <thead>
-//                 <tr>
-//                   <th>Exercise</th>
-//                   <th>Reps</th>
-//                   <th>Sets</th>
-                  
-//                 </tr>
-//               </thead>
-//               <tbody>
-//                 {exercises.map((exercise, index) => (
-//                   <tr key={index}>
-//                     <td>
-//                       <input
-//                         type="text"
-//                         value={exercise.exercise}
-//                         onChange={(e) => handleExerciseChange(index, 'exercise', e.target.value)}
-//                         className="form-input" // Add your input class here
-//                       />
-//                     </td>
-//                     <td>
-//                       <input
-//                         type="number"
-//                         value={exercise.reps}
-//                         onChange={(e) => handleExerciseChange(index, 'reps', e.target.value)}
-//                         className="form-input" // Add your input class here
-//                       />
-//                     </td>
-//                     <td>
-//                       <input
-//                         type="number"
-//                         value={exercise.sets}
-//                         onChange={(e) => handleExerciseChange(index, 'sets', e.target.value)}
-//                         className="form-input" // Add your input class here
-//                       />
-//                     </td>
-//                     <td>
-//                         <button type="button" onClick={() => handleDeleteExercise(index)} className="delete-btn">
-//                             <img src={DeleteIcon} alt="Delete" className="delete-icon" height="20px" width="20px"/> {/* Add your img tag here */}
-//                         </button>
-//                     </td>
-//                   </tr>
-//                 ))}
-//               </tbody>
-//             </table>
-//                         <button type="button" onClick={handleAddExercise} className="plus-btn">
-//                             <img src={AddIcon} alt="Add" className="plus-icon" height="30px" width="30px"/> {/* Add your img tag here */}
-//                         </button>          
-//             </div>
-//           <div>
-//             <button type="button" onClick={handleSaveWorkout} className="submit-btn">Save Workout Plan</button> {/* Add your button class here */}
-//             <button type="button" onClick={handleDownloadPDF} className="submit-btn">Download as PDF</button> {/* Add your button class here */}
-//           </div>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default WorkoutForm;
 import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
@@ -196,7 +36,7 @@ const WorkoutForm = () => {
   };
 
   const handleSaveWorkout = () => {
-    axios.post('http://localhost:8070/api/workout-plans/api/workout/save', {
+    axios.post('http://localhost:8080/api/workout-plans/api/workout/save', {
       name,
       age,
       weight,
@@ -273,7 +113,7 @@ const WorkoutForm = () => {
         />
       <div className="form-container">
       
-        <h2 className='meal-plan-form .head h2'>Create New Workout Plan</h2>
+        <h2 className='header'>Create New Workout Plan</h2>
         
         <form className="form-sec workout-form">
           <div className="form-group">
@@ -338,6 +178,7 @@ const WorkoutForm = () => {
                         value={exercise.exercise}
                         onChange={(e) => handleExerciseChange(index, 'exercise', e.target.value)}
                         className="form-input"
+                        
                       />
                     </td>
                     <td>
@@ -357,21 +198,21 @@ const WorkoutForm = () => {
                       />
                     </td>
                     <td>
-                      <button type="button" onClick={() => handleDeleteExercise(index)} className="delete-btn">
-                        <img src={DeleteIcon} alt="Delete" className="delete-icon" height="20px" width="20px"/>
-                      </button>
+                     
+                        <img src={DeleteIcon} alt="Delete" className="delete-icon" height="20px" width="20px" type="button" onClick={() => handleDeleteExercise(index)}/>
+                      
                     </td>
                   </tr>
                 ))}
               </tbody>
             </table>
-            <button type="button" onClick={handleAddExercise} className="plus-btn">
-              <img src={AddIcon} alt="Add" className="plus-icon" height="30px" width="30px"/>
-            </button>
+            
+              <img src={AddIcon} alt="Add" className="plus-icon" height="30px" width="30px" type="button" onClick={handleAddExercise}/>
+            
           </div>
           <div>
-            <button type="button" onClick={handleSaveWorkout} className="submit-btn">Save Workout Plan</button>
-            <button type="button" onClick={handleDownloadPDF} className="submit-btn">Download as PDF</button>
+            <button type="button" onClick={handleSaveWorkout} className="btn-submit">Save Workout Plan</button>
+            <button type="button" onClick={handleDownloadPDF} className="btn-submit">Download as PDF</button>
           </div>
         </form>
       </div>
